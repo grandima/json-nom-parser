7fc618623270b3d596281d5fc8269942e7156595
Dmytro Medynskyi <grandima@gmail.com>
2024-01-03 01:08:46 +0100

    add test optimizations

--- benchmarks/benches/json.rs
+++ benchmarks/benches/json.rs
@@ -3,13 +3,14 @@ extern crate criterion;
 
 use json_nom_parser::parse_json;
 use criterion::Criterion;
-
+#[global_allocator]
+static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
+static NETWORK_DATA: &str = include_str!("../network_data.json");
 fn bench_parse_json(c: &mut Criterion) {
     c.bench_function("parse specific json benchmark", |b|{
         b.iter(||{
             let mut map = json_nom_parser::KEY_VALUE_SIZE.clone();
-            let input = std::fs::read_to_string("network_data.json").unwrap();
-            _ = parse_json(&mut map)(&input);
+            _ = parse_json(&mut map)(NETWORK_DATA);
         });
     });
 }
